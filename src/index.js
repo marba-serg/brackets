bc3932b89894c9cdf93031ccf756b46998952934
marba-serg <68277911+marba-serg@users.noreply.github.com>
2022-01-27 09:59:13 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrStr = str.split('');
+  for (let i = 0; i < arrStr.length; i++) {
+    bracketsConfig.forEach((bracketsPare) => {
+      if (arrStr[i] === bracketsPare[0] && arrStr[i + 1] === bracketsPare[1]) {
+        arrStr.splice(i, 2);
+        i = -1;
+      }
+    })
+  }
+
+  return !arrStr.length
 }
